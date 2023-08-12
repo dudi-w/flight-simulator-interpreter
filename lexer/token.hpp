65e7647a4ebd202383d35fc61e4ae1a5021e14cc
itzhak pintel (mefathimtech19-Latitude-7490) <izpintel@gmail.com>
2023-08-12 21:42:48 +0300

    token

--- lexer/token.hpp
+++ lexer/token.hpp
@@ -1,14 +1,29 @@
 #ifndef TOKEN_HPP
 #define TOKEN_HPP
+
 #include <string>
 #include "token_enum.hpp"
+
 namespace fgi {
 namespace lexer {
 
-struct Token {
+class Token {
+public:
     Token(Token const& other) = delete;
     Token& operator=(Token const& other) = delete;
+    explicit Token(
+        TokenType type,
+        std::string str,
+        size_t row,
+        size_t column
+    );
 
+    TokenType type() const;
+    std::string const& str() const;
+    size_t row() const;
+    size_t column() const;
+
+private:
     TokenType m_type;
     std::string m_str;
     size_t m_row;
