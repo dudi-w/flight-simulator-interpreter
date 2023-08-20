d512d0d413014365a9249e6e5cdb8221cec5a7f7
itzhak pintel (mefathimtech19-Latitude-7490) <izpintel@gmail.com>
2023-08-20 15:24:05 +0300

    rename main.cpp --> lexer/lexer_test

---
+++ lexer/lexer_test.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "lexer.hpp"
+#include "token.hpp"
+
+int main()
+{
+    auto fs = std::ifstream("flight_instructions.txt");
+    std::stringstream ss;
+    std::string c;
+    ss << fs.rdbuf();
+    c = ss.str();
+
+    fgi::lexer::Lexer lexer;
+    auto v = lexer.tokenize(c);
+    for(auto const& t: v){
+        std::cout 
+            << "row: " << t.row()
+            << ", clolumn: " << t.column()
+            << ", type: " << static_cast<int>(t.type())
+            << ", str: " << t.str()
+            << std::endl;
+    }
+
+    
+    return 0;
+}
