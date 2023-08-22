576821aa6bd556c79e0ac0814f5fc9a59782e571
ushi-ah <asher.a48@gmail.com>
2023-08-22 16:39:09 +0300

    update expression tokens

---
+++ Lexer/token.hpp
@@ -0,0 +1,35 @@
+#ifndef TOKEN_HPP
+#define TOKEN_HPP
+
+#include <string>
+#include "token_enum.hpp"
+
+namespace fgi{
+namespace lexer {
+
+class Token {
+public:
+    Token(Token const& other) = default;
+    Token& operator=(Token const& other) = default;
+    explicit Token(
+        TokenType type,
+        std::string str,
+        size_t row,
+        size_t column
+    );
+
+    TokenType type() const;
+    std::string const& str() const;
+    size_t row() const;
+    size_t column() const;
+
+private:
+    TokenType m_type;
+    std::string m_str;
+    size_t m_row;
+    size_t m_column;
+};
+
+} // namespace lexer
+} // namespace fgi
+#endif
