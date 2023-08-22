fad04168f9229746643b385dc0883f501ea5f977
ushi-ah <asher.a48@gmail.com>
2023-08-22 17:23:23 +0300

    Fix operator comparisons

--- ExpressionTokens/expressionParser.cpp
+++ ExpressionTokens/expressionParser.cpp
@@ -1,4 +1,5 @@
 #include "expressionParser.hpp"
+#include "./Lexer/token_enum.hpp"
 
 using namespace fgi;
 using namespace Parser;
@@ -14,11 +15,11 @@ float ExpressionParser::parse()
 float ExpressionParser::parseAddSub()
 {
     float left = parseMulDiv();
-    while (m_startToken != m_endToken && (m_startToken->m_str == "+" || m_startToken->m_str == "-") && m_startToken->m_row == m_row) {
-        std::string op = m_startToken->m_str;
+    while (m_startToken != m_endToken && (m_startToken->m_type == lexer::TokenType::Add || m_startToken->m_type == lexer::TokenType::Sub) && m_startToken->m_row == m_row) {
+        lexer::TokenType op = m_startToken->m_type;
         ++m_startToken;
         float right = parseMulDiv();
-        if (op == "+") {
+        if (op == lexer::TokenType::Add) {
             left += right;
         } else {
             left -= right;
@@ -30,11 +31,11 @@ float ExpressionParser::parseAddSub()
 float ExpressionParser::parseMulDiv()
 {
     float left = parseNumber();
-    while (m_startToken != m_endToken && (m_startToken->m_str == "*" || m_startToken->m_str == "/") && m_startToken->m_row == m_row) {
-        std::string op = m_startToken->m_str;
+    while (m_startToken != m_endToken && (m_startToken->m_type == lexer::TokenType::Mul || m_startToken->m_type == lexer::TokenType::Div) && m_startToken->m_row == m_row) {
+        lexer::TokenType op = m_startToken->m_type;
         ++m_startToken;
         float right = parseNumber();
-        if (op == "*") {
+        if (op == lexer::TokenType::Mul) {
             left *= right;
         } else {
             left /= right;
@@ -49,16 +50,16 @@ float ExpressionParser::parseNumber()
         throw std::runtime_error("Unexpected end of input.");
     }
 
-    if (m_startToken->m_str == "-") {
+    if (m_startToken->m_type == lexer::TokenType::Sub) {
         ++m_startToken;
         return -parseNumber();
     }
 
-    if (m_startToken->m_str == "(" && m_startToken->m_row == m_row) {
+    if (m_startToken->m_type == lexer::TokenType::LeftBracket && m_startToken->m_row == m_row) {
         ++m_parenLevel;
         ++m_startToken;
         float result = parseAddSub();
-        if (m_startToken == m_endToken || m_startToken->m_str != ")" || m_startToken->m_row != m_row) {
+        if (m_startToken == m_endToken || m_startToken->m_type != lexer::TokenType::RightBracket || m_startToken->m_row != m_row) {
             throw std::runtime_error("Expected opening parenthesis.");
         }
         --m_parenLevel;
