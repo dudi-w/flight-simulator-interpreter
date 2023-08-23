bb4dac6d1fa001fb80a7273740ef2bb25575fd34
nahumnahum <641724ab@gmail.com>
2023-08-23 14:30:02 +0300

    only expression files

---
+++ expression/mul.hpp
@@ -0,0 +1,27 @@
+#ifndef MUL_HPP
+#define MUL_HPP
+
+#include <memory>
+
+#include "expression.hpp"
+
+namespace fg { // namespace flight gear
+
+class Mul : public IExpression
+{
+public:
+    explicit Mul(std::shared_ptr<IExpression> const& first, std::shared_ptr<IExpression> const& second);
+    Mul(Mul const& other) = default;
+    Mul& operator=(Mul const& other) = default;
+    ~Mul() = default;
+
+    float get_value() const override;
+
+private:
+    std::shared_ptr<IExpression> m_first;
+    std::shared_ptr<IExpression> m_second;
+};
+
+} //namespace
+
+#endif
