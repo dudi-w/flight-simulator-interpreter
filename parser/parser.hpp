7df4ecb05a21ee7fc3750bc52d40589527070dc8
itzhak pintel (mefathimtech19-Latitude-7490) <izpintel@gmail.com>
2023-08-24 02:25:56 +0300

    more command builders

--- parser/parser.hpp
+++ parser/parser.hpp
@@ -23,8 +23,33 @@ public:
     Parser& operator=(Parser const& other) = delete;
     Parser() = default;
 
-    static std::unique_ptr<Command> parse(std::vector<lexer::Token> const& tokens);
-    static std::unique_ptr<Command> parse(TokensItr it, TokensItr end);
+    std::unique_ptr<Command> parse(std::vector<lexer::Token> const& tokens) const;
+
+private:
+    static std::unique_ptr<Command> parse(TokensItr&, TokensItr);
+    static std::unique_ptr<Expression> build_expression(TokensItr& it, TokensItr end);
+
+    // commands builders
+    static std::unique_ptr<Command> codeBlock_builder(TokensItr& it, TokensItr end);
+    static std::unique_ptr<Command> connect_builder(TokensItr& it, TokensItr end);
+    static std::unique_ptr<Command> openDataServer_builder(TokensItr& it, TokensItr end);
+    static std::unique_ptr<Command> print_builder(TokensItr& it, TokensItr end);
+    static std::unique_ptr<Command> sleep_builder(TokensItr& it, TokensItr end);
+    static std::unique_ptr<Command> var_heandler(TokensItr& it, TokensItr end);
+    static std::unique_ptr<Command> while_builder(TokensItr& it, TokensItr end);
+
+
+
+    using BuilderFunc = std::unique_ptr<Command>(*)(TokensItr&, TokensItr);
+    static inline std::unordered_map<lexer::TokenType, BuilderFunc> m_buildersMap = {
+        {lexer::TokenType::Connect, connect_builder},
+        {lexer::TokenType::OpenDataServer, openDataServer_builder},
+        {lexer::TokenType::Print, print_builder},
+        {lexer::TokenType::Print, sleep_builder},
+        {lexer::TokenType::Print, var_heandler},
+        {lexer::TokenType::Print, while_builder},
+        {lexer::TokenType::LeftCurlyBracket, codeBlock_builder}
+    };
 };
 
 } // namespace parser
