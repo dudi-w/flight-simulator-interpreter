ec3a61792619162b45b3342201d363b0aba11ff5
nahumnahum <641724ab@gmail.com>
2023-08-23 14:30:02 +0300

    only expression files

---
+++ expression/add.hpp
@@ -0,0 +1,27 @@
+#ifndef ADD_HPP
+#define ADD_HPP
+
+#include <memory>
+
+#include "expression.hpp"
+
+namespace fg { // namespace flight gear
+
+class Add : public IExpression
+{
+public:
+    explicit Add(std::shared_ptr<IExpression> const& first, std::shared_ptr<IExpression> const& second);
+    Add(Add const& other) = default;
+    Add& operator=(Add const& other) = default;
+    ~Add() = default;
+
+    float get_value() const override;
+
+private:
+    std::shared_ptr<IExpression> m_first;
+    std::shared_ptr<IExpression> m_second;
+};
+
+} //namespace
+
+#endif
