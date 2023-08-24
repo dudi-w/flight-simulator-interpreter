390079789a074941c68c882cb0024c04b6179289
nahumnahum <641724ab@gmail.com>
2023-08-24 16:42:14 +0300

    variable expression

---
+++ expression/variable_expression.hpp
@@ -0,0 +1,26 @@
+#ifndef VARIABLE_EXPRESSION_HPP
+#define VARIABLE_EXPRESSION_HPP
+
+#include <string>
+
+#include "expression.hpp"
+
+namespace fg { // namespace flight gear
+
+class VariableExpression : public IExpression
+{
+public:
+    explicit VariableExpression(std::string const& variable_name);
+    VariableExpression(VariableExpression const& other) = default;
+    VariableExpression& operator=(VariableExpression const& other) = default;
+    ~VariableExpression() = default;
+
+    float get_value() const override;
+
+private:
+    std::string m_variable_name;
+};
+
+} //namespace
+
+#endif
