57740f54d4161d4a325780bb87a184689d2de6b0
nahumnahum <641724ab@gmail.com>
2023-08-24 16:42:14 +0300

    variable expression

---
+++ expression/variable_expression.cpp
@@ -0,0 +1,17 @@
+#include "variable_expression.hpp"
+#include "environment.hpp"
+
+namespace fg { // namespace flight gear
+
+VariableExpression::VariableExpression(std::string const& variable_name)
+: m_variable_name(variable_name)
+{}
+
+float VariableExpression::get_value() const
+{
+    Environment& environment = Environment::get_instance(std::cout);
+    float value = environment.get_variable(m_variable_name).get_value();
+    return value;
+}
+
+} //namespace
