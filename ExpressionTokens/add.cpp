2ff7241f0ad8277802d043a0771518d1a64a21ff
ushi-ah <asher.a48@gmail.com>
2023-08-24 18:44:42 +0300

    expressionParser branch

---
+++ ExpressionTokens/add.cpp
@@ -0,0 +1,17 @@
+#include "add.hpp"
+
+namespace fgi { // namespace flight gear
+
+Add::Add(std::shared_ptr<IExpression> const& first, std::shared_ptr<IExpression> const& second)
+: m_first(first)
+, m_second(second)
+{}
+
+float Add::get_value() const
+{
+    float first_value = m_first->get_value();
+    float second_value = m_second->get_value();
+    return first_value + second_value;
+}
+
+} //namespace
---
+++ ExpressionTokens/literal.hpp
@@ -0,0 +1,26 @@
+#ifndef LITERAL_HPP
+#define LITERAL_HPP
+
+#include <string>
+
+#include "expression.hpp"
+
+namespace fgi { // namespace flight gear
+
+class Literal : public IExpression
+{
+public:
+    explicit Literal(std::string const& num);
+    Literal(Literal const& other) = default;
+    Literal& operator=(Literal const& other) = default;
+    ~Literal() = default;
+
+    float get_value() const override;
+
+private:
+    float m_num;
+};
+
+} //namespace
+
+#endif
