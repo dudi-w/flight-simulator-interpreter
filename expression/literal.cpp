4e1ec0a52b6b680982f7727099807b1863665243
nahumnahum <641724ab@gmail.com>
2023-08-30 15:51:39 +0300

    namespace

--- expression/literal.cpp
+++ expression/literal.cpp
@@ -1,10 +1,14 @@
 #include "literal.hpp"
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 namespace fp { // namespace flight plan
 =======
 namespace fp { // namespace flight gear
 >>>>>>> dd66aa6 (name space)
+=======
+namespace fp { // namespace flight plan
+>>>>>>> 25583f8 (namespace)
 namespace exp { // namespace exp
 
 Literal::Literal(std::string const& num)
--- expression/sub.cpp
+++ expression/sub.cpp
@@ -2,6 +2,7 @@
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 namespace fp { // namespace flight plan
 namespace exp { // namespace exp
 =======
@@ -9,6 +10,9 @@ namespace fg { // namespace flight gear
 >>>>>>> 07320af (only expression files)
 =======
 namespace fp { // namespace flight gear
+=======
+namespace fp { // namespace flight plan
+>>>>>>> 25583f8 (namespace)
 namespace exp { // namespace exp
 >>>>>>> dd66aa6 (name space)
 
