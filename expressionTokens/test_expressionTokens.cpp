ad203ca0554e03c956b09e114ac65f95cbc9124b
ushi-ah <asher.a48@gmail.com>
2023-09-04 14:43:23 +0300

    fix expression parser

--- expressionTokens/test_expressionTokens.cpp
+++ expressionTokens/test_expressionTokens.cpp
@@ -4,32 +4,24 @@
 #include "../lexer/token_enum.hpp"
 #include "expressionParser.hpp"
 #include "expression.hpp"
+#include "../lexer/lexer.hpp"
 
-int main()
+int main(int argc, char** argv)
 {
-    fp::lexer::Token a0(fp::lexer::TokenType::LeftBracket, "(", 3, 10);
-    fp::lexer::Token a1(fp::lexer::TokenType::Add, "+", 3, 10);
-    fp::lexer::Token a2(fp::lexer::TokenType::Number, "10", 3, 10);
-    fp::lexer::Token a3(fp::lexer::TokenType::Add, "+", 3, 10);
-    fp::lexer::Token a4(fp::lexer::TokenType::Number, "5", 3, 10);
-    fp::lexer::Token a5(fp::lexer::TokenType::RightBracket, ")", 3, 10);
-    fp::lexer::Token a6(fp::lexer::TokenType::LeftCurlyBracket, "{", 3, 10);
-    fp::lexer::Token a7(fp::lexer::TokenType::Mul, "*", 3, 10);
-    fp::lexer::Token a8(fp::lexer::TokenType::Number, "5", 3, 10);
 
-    std::vector<fp::lexer::Token> myVector;
-    fp::lexer::Token myTokens[] = { a0, a2, a3, a4, a5, a6, a7, a8};
+	fp::lexer::Lexer lexer;
 
-    for (const auto& token : myTokens) {
-        myVector.push_back(token);
-    }
+    std::vector<fp::lexer::Token> myVector = lexer.tokenize(
+		    argc == 2 ? argv[1] : "1 -1 < 2 + 1 "
+		    );
+
 
     std::vector<fp::lexer::Token>::const_iterator beginIterator = myVector.cbegin();
     std::vector<fp::lexer::Token>::const_iterator endIterator = myVector.cend();
 
     fp::parser::ExpressionParser eParser(beginIterator, endIterator);
 
-    std::shared_ptr<fp::IExpression> expression = eParser.parse();
+    std::shared_ptr<fp::IExpression> expression = eParser.parse().first;
     
     std::cout << expression->get_value() << std::endl;
     
