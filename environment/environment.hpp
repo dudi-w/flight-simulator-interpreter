5b7a535e95515bda52a5e88f7922692d2ea38440
dudi-w <dudi.w9453@gmail.com>
2023-09-04 16:27:11 +0300

    Changed singleton functions

--- environment/environment.hpp
+++ environment/environment.hpp
@@ -4,10 +4,10 @@
 #include <iostream>
 #include <memory>
 
-#include "communication_mudule/netClient.hpp"
-#include "communication_mudule/netServer.hpp"
-#include "variable/variable_map.hpp"
-#include "variable/variable.hpp"
+#include "../communication_mudule/includes/netClient.hpp"
+#include "../communication_mudule/includes/simulatorServer.hpp"
+// #include "../variable/variable_map.hpp"
+// #include "../variable/variable.hpp"
 
 namespace fp { // namespace flight plan
 namespace env { // namespace env
@@ -15,29 +15,29 @@ namespace env { // namespace env
 class Environment
 {
 public:
-    Environment(Environment const& other) = delete;
-    Environment& operator=(Environment const& other) = delete;
     ~Environment() = default;
 
-    Environment& get_instance(std::ostream& output_stream)//???
+    static void initialization_client(std::string const& host, int port);
+    static void initialization_server(int port, int ups);   
 
-    void initialization_client(int host, int port);
-    void initialization_server(int port, int ups);   
-    
-    void insert_to_map(std::string const& variable_name, std::shared_ptr<IVariable> variable);
-    float get_variable_value(std::string const& variable_name) const;
-    void set_variable_value(std::string const& variable_name, float new_value);
-    
-    void set_output_stream(std::ostream& output_stream);
-    void print_str(std::string const& message);
-    void print_float(float number);
+    static void insert_to_map(std::string const& variable_name, std::shared_ptr<IVariable> variable);
+    static float get_variable_value(std::string const& variable_name);
+    static void set_variable_value(std::string const& variable_name, float new_value);
+
+    static void set_output_stream(std::ostream& output_stream);
+    static void print_str(std::string const& message);
+    static void print_float(float number);
 
 private:
-    Environment(std::ostream& output_stream);
+    Environment() = default;
+    Environment(Environment const& other) = delete;
+    Environment& operator=(Environment const& other) = delete;
+
+    static Environment& get_instance();
 
 private:
     net::TelnetetClient m_client;
-    net::NetServer m_server;
+    fp::env::SimulatorServer m_server;
     VariableMap m_variable_map;
     std::ostream& m_output_stream;
 };
