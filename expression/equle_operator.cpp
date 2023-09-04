aa313edb86698f26f290d7bb35eacdd463f49033
nahumnahum <641724ab@gmail.com>
2023-09-04 14:41:54 +0300

    Revert "delete all files"
    This reverts commit 6f12b0840849389f1c692067da683164fdaf6b3a.

---
+++ expression/equle_operator.cpp
@@ -0,0 +1,22 @@
+#include "equle_operator.hpp"
+
+namespace fp { // namespace flight plan
+namespace exp { // namespace exp
+
+EquleOperator::EquleOperator(std::shared_ptr<IExpression> const& first, std::shared_ptr<IExpression> const& second)
+: m_first(first)
+, m_second(second)
+{}
+
+float EquleOperator::get_value() const
+{
+    float first_value = m_first->get_value();
+    float second_value = m_second->get_value();
+    if(first_value == second_value) {
+        return 1;
+    }
+    return 0;
+}
+
+} //namespace exp
+} //namespace fp
