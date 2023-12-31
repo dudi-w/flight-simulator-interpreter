da876dd3b27affcb228094ec2f3517ca83408851
dudi-w <dudi.w9453@gmail.com>
2023-12-31 15:47:25 +0200

    add Notes

--- environment/dataMap.hpp
+++ environment/dataMap.hpp
@@ -18,7 +18,9 @@ public:
 
     inline void insert(std::string const& varPath){ m_map.insert({varPath,0});}
     inline void insert(std::string && varPath){ m_map.insert({std::move(varPath),0});}
+    //Remember: this function is only used by the server
     inline void set(std::string const& varPath, float value){ m_map.at(varPath) = value;}
+    //Remember: this function is only used by the server
     inline void set(std::string && varPath, float value){ m_map.at(std::move(varPath)) = value;}
     inline float get(std::string const& varPath){ return m_map.at(varPath);}
     inline float get(std::string && varPath){ return m_map.at(std::move(varPath));}
