29aba99502711bd2ba679a23f256ef480288ff7a
dudi-w <dudi.w9453@gmail.com>
2024-01-07 14:00:39 +0200

    fix bug in insert

--- environment/dataMap.hpp
+++ environment/dataMap.hpp
@@ -16,8 +16,8 @@ public:
     DataMap& operator=(DataMap const& other) = default;
     ~DataMap() = default;
 
-    inline void insert(std::string const& varPath){ m_map.insert({varPath,0});}
-    inline void insert(std::string && varPath){ m_map.insert({std::move(varPath),0});}
+    inline void insert(std::string const& varPath){ m_map.insert(std::pair<std::string,float>(varPath,0.0f));}
+    inline void insert(std::string && varPath){ m_map.insert(std::pair<std::string,float>{std::move(varPath),0.0f});}
     //Remember: this function is only used by the server
     inline void set(std::string const& varPath, float value){ m_map.at(varPath) = value;}
     //Remember: this function is only used by the server
