f32a7b29819b7005b39179f7cea4c92f21d33726
dudi-w <dudi.w9453@gmail.com>
2024-01-08 21:05:14 +0200

    deleted assignment operator , added "const" for get functions

--- environment/dataMap.hpp
+++ environment/dataMap.hpp
@@ -13,7 +13,7 @@ class DataMap
 public:
     DataMap() = default;
     DataMap(DataMap const& other) = default;
-    DataMap& operator=(DataMap const& other) = default;
+    DataMap& operator=(DataMap const& other) = delete;
     ~DataMap() = default;
 
     inline void insert(std::string const& varPath){ m_map.insert(std::pair<std::string,float>(varPath,0.0f));}
@@ -22,8 +22,8 @@ public:
     inline void set(std::string const& varPath, float value){ m_map.at(varPath) = value;}
     //Remember: this function is only used by the server
     inline void set(std::string && varPath, float value){ m_map.at(std::move(varPath)) = value;}
-    inline float get(std::string const& varPath){ return m_map.at(varPath);}
-    inline float get(std::string && varPath){ return m_map.at(std::move(varPath));}
+    inline float get(std::string const& varPath) const { return m_map.at(varPath);}
+    inline float get(std::string && varPath) const { return m_map.at(std::move(varPath));}
 
 private:
     std::unordered_map<std::string , std::atomic<float>> m_map;
