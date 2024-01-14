3791a242a5fa4b16d831ee26b3b78b0e40fc6ac3
itzhak pintel (mefathimtech19-Latitude-7490) <izpintel@gmail.com>
2024-01-14 21:28:57 +0200

    main can get input from pipe

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <unistd.h>
+
 #include "lexer.hpp"
 #include "token.hpp"
 
@@ -9,41 +11,30 @@
 
 int main1(int argc, char *argv[])
 {
-    if(argc != 2){
-        std::cerr << "Usage: " << argv[0] << " " << "<path to flight instructions file>.";
-        exit(-1);
-    }
-    auto fs = std::ifstream(argv[1]);
     std::stringstream ss;
-    std::string c;
-    ss << fs.rdbuf();
-    c = ss.str();
+    if(isatty(STDIN_FILENO)){
+        if(argc != 2){
+            std::cerr << "Usage: " << argv[0] << " " << "<path to flight instructions file>.";
+            exit(-1);
+        } else {
+            auto fs = std::ifstream(argv[1]);
+            ss << fs.rdbuf();
+        }
+    } else {
+        ss << std::cin.rdbuf();
+    }
+    std::string code = ss.str();
 
     fp::lexer::Lexer lexer;
-    auto v = lexer.tokenize(c);
-    fp::parser::Parser parser;
-    auto out = parser.parse(v);
-    std::cout << "parser done" << std::endl;
+    auto tokens = lexer.tokenize(code);
+    auto out = fp::parser::Parser::parse(tokens);
 
     out->execute();
-    std::cout << "done" << std::endl;
-
-    // for(fp::lexer::Token const& t: v){
-    //     std::cout 
-    //         << "row: " << t.row()
-    //         << ", clolumn: " << t.column()
-    //         << ", type: " << static_cast<int>(t.type())
-    //         << ", str: " << t.str()
-    //         << std::endl;
-    // }
-
-
-    
+  
     return 0;
 }
 
 #include "simulator.hpp"
-
 int main2([[maybe_unused]]int argc, [[maybe_unused]]char *argv[]){
     fp::env::SimulatorControl sc;
     sc.set_server_port(1234);
@@ -59,29 +50,19 @@ int main2([[maybe_unused]]int argc, [[maybe_unused]]char *argv[]){
 
 int main3(int argc, char** argv)
 {
-
 	fp::lexer::Lexer lexer;
 
     std::vector<fp::lexer::Token> myVector = lexer.tokenize(
         argc == 2 ? argv[1] : "1 + 1/1 + 1/(1*2) + 1/(1*2*3) + 1/(1*2*3*4) + 1/(1*2*3*4*5)"
 	);
 
-
     std::vector<fp::lexer::Token>::const_iterator beginIterator = myVector.cbegin();
     std::vector<fp::lexer::Token>::const_iterator endIterator = myVector.cend();
-
     fp::parser::ExpressionParser eParser(beginIterator, endIterator);
-
     std::unique_ptr<fp::exp::IExpression> expression = eParser.parse().first;
-    
     std::cout << expression->get_value() << std::endl;
-    
     return 0;
 }
-
-
-
-
 
 int main(int argc, char *argv[]){
     return main1(argc, argv);
