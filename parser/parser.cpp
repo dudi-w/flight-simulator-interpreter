68700258ebf7d5ba65500f7b5ae9c646238e43fd
dudi-w <dudi.w9453@gmail.com>
2024-01-22 15:27:26 +0200

    without handling the size of the vector

--- parser/parser.cpp
+++ parser/parser.cpp
@@ -19,18 +19,11 @@ std::unique_ptr<com::Command> Parser::parse(TokensItr it, TokensItr end)
 {
     std::vector<std::unique_ptr<com::Command>> commands;
     if(it < end){
-        ssize_t estimated_vector_size = std::ceil(((end-1)->row() - it->row())*0.7);
-        commands.reserve(estimated_vector_size);
-
         std::unique_ptr<com::Command> comm;
         while (it != end)
         {
             std::tie(comm, it) = CommandsFactory::create(it, end);
             commands.emplace_back( std::move(comm) );
-        }
-
-        if(commands.capacity()*2 > commands.size()*3){
-            commands.shrink_to_fit();
         }
     }
     return std::make_unique<com::CodeBlockCommand>(std::move(commands));
